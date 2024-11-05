c94e48022686fd836ba8a53a58e64830baac24ba
xastaryx <someoneidunnook@gmail.com>
2024-11-05 18:22:47 +0300

    Реализована функция encrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,25 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    repeats = (len(plaintext) // len(keyword) + 1) * keyword
+    rez = repeats[:len(plaintext)]
+    index = 0
+    for i in plaintext:
+        if ord('a') > ord(i.lower()) or ord(i.lower()) > ord('z'):
+            ciphertext += i
+            index += 1
+        else:
+            k = rez[index]
+            shift = ord(k.lower()) - ord('a')
+            if ord(i.lower()) + shift <= ord('z'):
+                index += 1
+                ciphertext += chr(ord(i) + shift)
+            else:
+                if i.isupper():
+                    ciphertext += (chr(ord('a') + (ord(i.lower()) + shift - ord('z')) - 1)).upper()
+                if i.islower():
+                    ciphertext += (chr(ord('a') + (ord(i) + shift - ord('z')) - 1)).lower()
+                index += 1
     return ciphertext
 
 
@@ -24,5 +42,23 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    repeats = (len(ciphertext) // len(keyword) + 1) * keyword
+    rez = repeats[:len(ciphertext)]
+    index = 0
+    for i in ciphertext:
+        if ord('a') > ord(i.lower()) or ord(i.lower()) > ord('z'):
+            plaintext += i
+            index += 1
+        else:
+            k = rez[index]
+            shift = ord(k.lower()) - ord('a')
+            if ord(i.lower()) - shift >= ord('a'):
+                index += 1
+                plaintext += chr(ord(i) - shift)
+            else:
+                if i.isupper():
+                    plaintext += (chr(ord(i) - ord('a') + ord('z') - shift + 1)).upper()
+                if i.islower():
+                    plaintext += (chr(ord(i) - ord('a') + ord('z') - shift + 1)).lower()
+                index += 1
     return plaintext
